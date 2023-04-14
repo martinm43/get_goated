c28473f11b3c1df44fde5a16b633ecc625dcef83
MA Miller <miller.m.43@gmail.com>
2023-04-14 12:05:48 -0400

    updating the selenium statement used in tutorial - see StackOverflow 69875125

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,7 @@
 #Functional tests require stories. This is why both Robot and Testflows are good for functional testing
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 import time
 import unittest
 
@@ -19,11 +20,11 @@ class NewVisitorTest(unittest.TestCase):
 
         # She notices the page title and header mention to-do lists
         self.assertIn('To-Do', self.browser.title)
-        header_text = self.browser.find_element_by_tag_name('h1').text  
+        header_text = self.browser.find_element(By.TAG_NAME,'h1').text  
         self.assertIn('To-Do', header_text)
 
         # She is invited to enter a to-do item straight away
-        inputbox = self.browser.find_element_by_id('id_new_item')  
+        inputbox = self.browser.find_element(By.ID,'id_new_item')  
         self.assertEqual(
             inputbox.get_attribute('placeholder'),
             'Enter a to-do item'
@@ -38,8 +39,8 @@ class NewVisitorTest(unittest.TestCase):
         inputbox.send_keys(Keys.ENTER)  
         time.sleep(1)  
 
-        table = self.browser.find_element_by_id('id_list_table')
-        rows = table.find_elements_by_tag_name('tr')  
+        table = self.browser.find_element(By.ID,'id_list_table')
+        rows = table.find_elements(By.TAG_NAME,'tr')  
         self.assertTrue(
             any(row.text == '1: Buy peacock feathers' for row in rows)
         )
