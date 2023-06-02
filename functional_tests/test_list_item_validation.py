93d89a3dba0a921ef202ba0b0d1a6d4e5d68dc54
MA Miller <miller.m.43@gmail.com>
2023-06-02 19:19:44 -0400

    modification to unit and functional tests

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -19,6 +19,7 @@ class ItemValidationTest(FunctionalTest):
 
         # The home page refreshes, and there is an error message saying
         # that list items cannot be blank
+        #think of this as a cousin of the "wait 5 secs before making requests"
         self.wait_for(lambda: self.assertEqual(  
         self.browser.find_element_by_css_selector('.has-error').text,
         "You can't have an empty list item"
