3046714ae51a8793a62e05ad5db4638ce522889f
MA Miller <miller.m.43@gmail.com>
2023-06-03 09:37:33 -0400

    End of Validation at the Database Level chapter

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,6 @@
+from django.core.exceptions import ValidationError
 from django.shortcuts import redirect, render
 from lists.models import Item, List
-from django.core.exceptions import ValidationError
 
 def home_page(request):
     return render(request, 'home.html')
@@ -13,18 +13,25 @@ def new_list(request):
         item.full_clean()
         item.save()
     except ValidationError:
+        list_.delete()
         error = "You can't have an empty list item"
-        return render(request, 'home.html', {"error": error}) 
-    return redirect(f'/lists/{list_.id}/')
-
-
-def add_item(request, list_id):
-    list_ = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST['item_text'], list=list_)
-    return redirect(f'/lists/{list_.id}/')
+        return render(request, 'home.html', {"error": error})
+    return redirect(list_)
 
 
 def view_list(request, list_id):
     list_ = List.objects.get(id=list_id)
-    return render(request, 'list.html', {'list': list_})
+    error = None
+
+    if request.method == 'POST':
+        try:
+
+            item = Item(text=request.POST['item_text'], list=list_)
+            item.full_clean()
+            item.save()
+            return redirect(list_)
+        except ValidationError:
+            error = "You can't have an empty list item"
+
+    return render(request, 'list.html', {'list': list_, 'error': error})
 
